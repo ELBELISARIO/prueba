fdd76005c6eec97ba8502315e4e4c784f9d9baab
bdII <unsis.b@gmail.com>
2018-10-05 10:17:09 -0500

    Añadiendo completo la base de datos.

--- script.sql
+++ script.sql
@@ -1,2 +1,39 @@
 create database Tienda;
-	use Tienda;
+
+CREATE TABLE PROVEEDORES(
+NOMBRE varchar(20),
+DIRECCION varchar(60),
+TELEFONO varchar(10),
+ID_PROVEEDOR varchar(10) primary key);
+
+CREATE TABLE CATEGORIAS(
+ID_CATEGORIA varchar(10) primary key,
+DESCRIPCION varchar(100));
+
+CREATE TABLE CLIENTES(
+NOMBRE varchar(20),
+DIRECCION varchar(60),
+TELEFONO varchar(10),
+ID_CLIENTE varchar(10) primary key);
+
+CREATE TABLE FACTURAS(
+ID_FACTURA varchar(10) primary key,
+FECHA Date,
+Foreign key(Id_cliente) references CLIENTES(ID_CLIENTE));
+
+
+CREATE TABLE PRODUCTOS(
+ID_PRODUCTO varchar(10),
+DESCRIPCION varchar(100),
+PRECIO integer,
+Foreign key(Id_categoria) references CATEGORIAS(ID_CATEGORIA),
+Foreign key(id_proveedor) references PROVEEDORES(ID_PROVEEDOR));
+
+CREATE TABLE VENTAS(
+ID_VENTA varchar(10),
+CANTIDAD integer,
+Foreign key(Id_factura) references FACTURAS(ID_FACTURA),
+Foreign key(Id_producto) references PRODUCTOS(ID_PRODUCTO));
+
+
+
